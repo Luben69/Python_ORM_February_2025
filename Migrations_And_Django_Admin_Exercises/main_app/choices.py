4c7ab7a15241af6e095b6cc8b8a079fe1561e6c2
Luben69 <lpacharazov123@gmail.com>
2025-02-27 20:14:07 +0200

    removed .idea/

---
+++ Migrations_And_Django_Admin_Exercises/main_app/choices.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class OrderStatusChoices(models.TextChoices):
+    PENDING = 'Pending', 'Pending'
+    COMPLETED = 'Completed', 'Completed'
+    CANCELLED = 'Cancelled', 'Cancelled'
---
+++ Migrations_And_Django_Admin_Lab/main_app/admin.py
@@ -0,0 +1,5 @@
+
+
+from django.contrib import admin
+from Migrations_And_Django_Admin_Lab.main_app.models import Product
+admin.site.register(Product)
